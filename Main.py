4587233aab790fb3d60c232b761e4a8d8d8ed759
thebrownkidd <139674706+thebrownkidd@users.noreply.github.com>
2024-04-24 11:33:57 +0530

    Added teacher login to fix

--- Main.py
+++ Main.py
@@ -4,25 +4,25 @@ from datetime import date
 today = date.today()
 obj = sql.connect('teacher.db')
 t_cur = obj.cursor()
-# while True:
-#     print("Please login....\n")
-#     t_user = int(input("Enter teacher id: "))
-#     t_pass = str(input("Enter teacher pass: "))
+while True:
+    print("Please login....\n")
+    t_user = int(input("Enter teacher id: "))
+    t_pass = str(input("Enter teacher pass: "))
 
-#     t_cur.execute("SELECT * FROM TEACHER WHERE T_ID = " + str(t_user)+ " AND TPASS = \""+ t_pass + "\"")
-#     out = t_cur.fetchall()
-#     if len(out) == 0:
-#         print("ERROR! INVALID LOGIN")
-#     else:
-#         print("\n\n\n\n\n")
-#         obj.close()
-#         new_obj = sql.connect('attendance.db')
-#         newcur = new_obj.cursor()
-#         today = today.strftime("%d%B")
-#         command = "ALTER TABLE ATTENDANCE ADD \"" + str(today) + "\" INT(1);"
-#         newcur.execute(command)
-#         new_obj.close()
-#         break
+    t_cur.execute("SELECT * FROM TEACHER WHERE T_ID = " + str(t_user)+ " AND TPASS = \""+ t_pass + "\"")
+    out = t_cur.fetchall()
+    if len(out) == 0:
+        print("ERROR! INVALID LOGIN")
+    else:
+        print("\n\n\n\n\n")
+        obj.close()
+        new_obj = sql.connect('attendance.db')
+        newcur = new_obj.cursor()
+        today = today.strftime("%d%B")
+        command = "ALTER TABLE ATTENDANCE ADD \"" + str(today) + "\" INT(1);"
+        newcur.execute(command)
+        new_obj.close()
+        break
 while True:
     print("Welocme to AMS, please select one of the following outputs: ")
     print("\nSno\t| Action\n0. \t| EXIT\n1. \t| View your attendance\n2. \t| Mark Attendance\n")
