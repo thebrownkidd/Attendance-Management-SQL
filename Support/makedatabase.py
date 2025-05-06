93f216835c0e2af194542cf5d921e48326202a6a
thebrownkidd <139674706+thebrownkidd@users.noreply.github.com>
2025-05-07 00:25:23 +0530

    Mod: Functionality study

--- Support/makedatabase.py
+++ Support/makedatabase.py
@@ -2,9 +2,21 @@ import sqlite3 as sql
 db = sql.connect('Databases/attendance.db')
 cur = db.cursor()
 # cur.execute("CREATE TABLE TEACHER (T_NAME CHAR, T_ID INT(4) PRIMARY KEY, TPASS CHAR NOT NULL)")
-cur.execute("ALTER TABLE ATTENDANCE DROP COLUMN \"29April\"")
+# cur.execute("SHOW ;")
+# cur.execute("DESC TABLE TEACHER;")
+# db.commit()
+# cur.execute("SELECT * FROM TEACHER")
+# entries = cur.fetchall()
+# for i in entries:
+#     print(i)
+# cur.execute("SELECT * FROM STUDENTS")
+# tables = cur.fetchall()
+# for table in tables:
+#     print(table)
+cur.execute('''INSERT INTO STUDENTS VALUES ('Aditya', 1235, "Pass123", 12, 'A')''')
+
 db.commit()
-cur.execute("SELECT * FROM ATTENDANCE")
-entries = cur.fetchall()
-for i in entries:
-    print(i)
+cur.execute("SELECT * FROM STUDENTS")
+tables = cur.fetchall()
+for table in tables:
+    print(table)
